bc29744d880a82e1fdd80d330dc1d55ab7628084
Nitsa Kanungo <kanungo.n@northeastern.edu>
2023-11-30 16:56:25 -0500

    set up basic game class

---
+++ lib/include/Game.h
@@ -0,0 +1,12 @@
+//
+// Created by Nitsa Kanungo on 11/30/23.
+//
+
+#ifndef SUDOKU_GAME_H
+#define SUDOKU_GAME_H
+
+class Game {
+
+};
+
+#endif //SUDOKU_GAME_H
---
+++ lib/src/Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nitsa Kanungo on 11/30/23.
+//
+
+#include "../include/Game.h"
