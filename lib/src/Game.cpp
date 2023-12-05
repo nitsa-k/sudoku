0f399df600a37cd54d6769705bd6199ec653e290
Nitsa Kanungo <kanungo.n@northeastern.edu>
2023-12-04 20:01:28 -0500

    formatting & comments

--- lib/src/Game.cpp
+++ lib/src/Game.cpp
@@ -12,7 +12,9 @@
 #include <random>
 #include <chrono>
 
+// constructor
 Game::Game() {
+    // sdl set up
     window = SDL_CreateWindow("Sudoku", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_SIZE, SCREEN_SIZE,
                               SDL_WINDOW_SHOWN);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
@@ -20,18 +22,21 @@ Game::Game() {
     TTF_Init();
     font = TTF_OpenFont("../../resources/ABeeZee-Regular.otf", 25);
 
+    // random seed for generation
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     srand(seed);
 
+    // no selected cell upon launch
     selectedRow = -1;
     selectedCol = -1;
 }
 
-void Game::saveGame(const std::string &filename) {
+// serializes & saves game state
+void Game::saveGame(const std::string &filename) const {
     std::ofstream file(filename);
 
     if (!file.is_open()) {
-        std::cerr << "Error: Unable to open file for saving." << std::endl;
+        std::cerr << "unable to open file for saving" << std::endl;
         return;
     }
 
@@ -40,11 +45,12 @@ void Game::saveGame(const std::string &filename) {
     file.close();
 }
 
+// loads previously saved game state
 void Game::loadGame(const std::string &filename) {
     std::ifstream file(filename);
 
     if (!file.is_open()) {
-        std::cerr << "Error: Unable to open file for loading." << std::endl;
+        std::cerr << "unable to open file for loading" << std::endl;
         return;
     }
 
@@ -53,43 +59,33 @@ void Game::loadGame(const std::string &filename) {
     file.close();
 }
 
-void Game::fillBox(int row, int col) {
-    std::vector<int> nums;
-
-    for (int i = 1; i <= GRID_SIZE; i++) {
-        nums.push_back(i);
-    }
-
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::shuffle(nums.begin(), nums.end(), std::default_random_engine(seed));
-
-    for (int i = 0; i < sqrt(GRID_SIZE); i++) {
-        for (int j = 0; j < sqrt(GRID_SIZE); j++) {
-            grid[i + row][j + col].setValue(nums.back());
-            nums.pop_back();
-        }
-    }
-}
-
+// checks if given row contains given number
 bool Game::rowContains(int row, int num, Cell checkGrid[GRID_SIZE][GRID_SIZE]) {
+    // traverse column indices of given row and check for any equal values
     for (int col = 0; col < GRID_SIZE; col++) {
         if (checkGrid[row][col].getValue() == num) return true;
     }
-    return false;
+
+    return false;   // value was not found
 }
 
+// checks if given column contains given number
 bool Game::colContains(int col, int num, Cell checkGrid[GRID_SIZE][GRID_SIZE]) {
+    // traverse row indices of given column and check for any equal values
     for (int row = 0; row < GRID_SIZE; row++) {
         if (checkGrid[row][col].getValue() == num) return true;
     }
-    return false;
+
+    return false;   // value was not found
 }
 
+// checks if box of the given [row][col] coordinate contains given number
 bool Game::boxContains(int row, int col, int num, Cell checkGrid[GRID_SIZE][GRID_SIZE]) {
     // find the top left of the box that grid[row][col] is in
     int topRow = (row / (int) (sqrt(GRID_SIZE))) * (int) (sqrt(GRID_SIZE));
     int leftCol = (col / (int) (sqrt(GRID_SIZE))) * (int) (sqrt(GRID_SIZE));
 
+    // traverse box and check for any equal values
     for (int i = 0; i < static_cast<int>(sqrt(GRID_SIZE)); i++) {
         for (int j = 0; j < static_cast<int>(sqrt(GRID_SIZE)); j++) {
             if (checkGrid[topRow + i][leftCol + j].getValue() == num) {
@@ -98,40 +94,66 @@ bool Game::boxContains(int row, int col, int num, Cell checkGrid[GRID_SIZE][GRID
         }
     }
 
-    return false;
+    return false;   // value was not found
 }
 
-
+// finds an empty cell in the grid
 bool Game::findEmpty(int &row, int &col, Cell checkGrid[GRID_SIZE][GRID_SIZE]) {
+    // traverses grid and checks for zero value
     for (row = 0; row < GRID_SIZE; row++) {
         for (col = 0; col < GRID_SIZE; col++) {
             if (checkGrid[row][col].getValue() == 0) return true;
         }
     }
-    return false;
+
+    return false;   // value was not found
 }
 
+// fills any empty cells in the grid
 bool Game::fillRest(Cell checkGrid[GRID_SIZE][GRID_SIZE]) {
     // uses a backtracking algorithm to fill the rest of the grid
     int row, col;
 
     if (!findEmpty(row, col, checkGrid)) {
-        return true;
+        return true;    // grid is full
     }
 
     for (int num = 1; num <= GRID_SIZE; num++) {
         if (!rowContains(row, num, checkGrid) && !colContains(col, num, checkGrid) &&
             !boxContains(row, col, num, checkGrid)) {
-            checkGrid[row][col].setValue(num);
-            if (fillRest(checkGrid)) return true;
-            checkGrid[row][col].setValue(0);
+            checkGrid[row][col].setValue(num);  // test a value
+            if (fillRest(checkGrid)) return true;   // check if rest of grid can be validly filled if this cell is
+                                                    // given this value
+            checkGrid[row][col].setValue(0);    // revert back to zero - tested value did not result in valid grid
         }
     }
 
-    return false;
+    return false;   // grid could not be filled validly
 }
 
+// fills a GRID_SIZE x GRID_SIZE box randomly
+void Game::fillBox(int row, int col) {
+    std::vector<int> nums;
 
+    // add numbers 1 - GRID_SIZE to the vector
+    for (int i = 1; i <= GRID_SIZE; i++) {
+        nums.push_back(i);
+    }
+
+    // randomly shuffle the vector
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::shuffle(nums.begin(), nums.end(), std::default_random_engine(seed));
+
+    // assign each cell in the box a random value from the vector
+    for (int i = 0; i < sqrt(GRID_SIZE); i++) {
+        for (int j = 0; j < sqrt(GRID_SIZE); j++) {
+            grid[i + row][j + col].setValue(nums.back());
+            nums.pop_back();
+        }
+    }
+}
+
+// checks if grid is solvable
 bool Game::solvable() {
     // create a copy so the grid is not edited
     Cell copy[GRID_SIZE][GRID_SIZE];
@@ -142,10 +164,12 @@ bool Game::solvable() {
         }
     }
 
-    return fillRest(copy);
+    return fillRest(copy);  // can the grid be filled with valid numbers?
 }
 
+// initializes grid
 void Game::initGrid() {
+    // init all cell values to zero and editable to false
     for (auto &i: grid) {
         for (auto &j: i) {
             j.setValue(0);
@@ -153,7 +177,7 @@ void Game::initGrid() {
         }
     }
 
-    // fill diagonal boxes with numbers
+    // fill diagonal boxes randomly with numbers if grid is big enough to not potentially cause problems
     if (GRID_SIZE > 4) {
         for (int i = 0; i < GRID_SIZE; i += (int) sqrt(GRID_SIZE)) {
             fillBox(i, i);
@@ -174,6 +198,7 @@ void Game::initGrid() {
             grid[row][col].setValue(0);
             grid[row][col].setEditable(true);
 
+            // if removing this number makes the puzzle unsolvable, replace it
             if (!solvable()) {
                 grid[row][col] = temp;
                 grid[row][col].setEditable(false);
@@ -184,11 +209,13 @@ void Game::initGrid() {
     }
 }
 
+// updates selected row and column with user click
 void Game::selectBox(int x, int y) {
     selectedRow = y / CELL_SIZE;
     selectedCol = x / CELL_SIZE;
 }
 
+// updates selected cell with user entered number if the cell is editable
 void Game::typeNum(SDL_Keycode key) {
     if (selectedRow != -1 && selectedCol != -1 && key >= SDLK_1 && key <= SDLK_9 &&
         grid[selectedRow][selectedCol].getEditable()) {
@@ -196,6 +223,7 @@ void Game::typeNum(SDL_Keycode key) {
     }
 }
 
+// draws the grid
 void Game::drawGrid() {
     // screen color
     SDL_SetRenderDrawColor(renderer, 237, 246, 249, 225);
@@ -245,7 +273,7 @@ void Game::drawGrid() {
         }
     }
 
-    // draw selected cell
+    // draw a box around the selected cell
     if (selectedRow != -1 && selectedCol != -1) {
         SDL_SetRenderDrawColor(renderer, 131, 197, 190, 255);
         SDL_Rect selected = {selectedCol * CELL_SIZE, selectedRow * CELL_SIZE, CELL_SIZE, CELL_SIZE};
@@ -255,22 +283,24 @@ void Game::drawGrid() {
     SDL_RenderPresent(renderer);
 }
 
+// checks if game has been won
 bool Game::checkWin() {
     for (int row = 0; row < GRID_SIZE; row++) {
         for (int col = 0; col < GRID_SIZE; col++) {
-            if (grid[row][col].getValue() == 0) return false;
+            if (grid[row][col].getValue() == 0) return false;   // there is still an empty box, so game not won yet
             int num = grid[row][col].getValue();
             grid[row][col].setValue(0);
             if (rowContains(row, num, grid) || colContains(col, num, grid) || boxContains(row, col, num, grid)) {
                 grid[row][col].setValue(num);
-                return false;
+                return false;   // a row, column, or box contain two of the same number
             }
             grid[row][col].setValue(num);
         }
     }
-    return true;
+    return true;    // checks passed, so game has been won
 }
 
+// displays winning game scene and allows user to exit or play again
 void Game::handleWin() {
     SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Congratulations!", "You won the game!", window);
 
@@ -292,6 +322,7 @@ void Game::handleWin() {
     }
 }
 
+// displays options menu
 int Game::showMenu() {
     const SDL_MessageBoxButtonData buttons[] = {
             {0,                                       0, "New Game"},
@@ -311,6 +342,7 @@ int Game::showMenu() {
     return choice;
 }
 
+// launches and runs the game
 void Game::run() {
     bool exit = false;
     bool menu = true;
